e5dfd128a0c873bf7e958be4c6cf9c2e0f7fa437
chedeline <vchedeline@gmail.com>
2022-07-16 21:00:07 -0400

    adds models and updates timezone

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Kdrama(models.Model):
+  title = models.CharField(max_length=100)
+  year = models.PositiveIntegerField()
+  genre = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+
+  def __str__(self):
+    return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Kdrama
+# class Kdrama:
+#   def __init__(self, title, year, genre, description):
+#     self.title = title
+#     self.year = year
+#     self.genre = genre
+#     self.description = description
 
-class Kdrama:
-  def __init__(self, title, year, genre, description):
-    self.title = title
-    self.year = year
-    self.genre = genre
-    self.description = description
-
-kdramas = [
-  Kdrama('Boys over Flowers', 2009, 'Romantic', 'Unassuming high school girl Jan-di stands up to -- and eventually falls for -- a spoiled rich kid who belongs to the school''s most powerful clique'),
-  Kdrama('My Secret Romance', 2017, 'Romantic', 'Jin Wook, a rich playboy set to inherit his dad''s company, has a chance encounter with Yoo Mi, who has more modest ambitions to become a nutritionist'),
-  Kdrama('Inheritors', 2013, 'Romantic', 'After a chance encouter in LA, two teens from different social backgrounds reunite at an exclusive high school attended by Korea''s uber rich'),
-  Kdrama('Love Alarm', 2021, 'Romantic', 'In a world where an app alerts people if someone in the vicinity likes them, Kim Jojo experiences young love while coping with personal adversities')
-]
+# kdramas = [
+#   Kdrama('Boys over Flowers', 2009, 'Romantic', 'Unassuming high school girl Jan-di stands up to -- and eventually falls for -- a spoiled rich kid who belongs to the school''s most powerful clique'),
+#   Kdrama('My Secret Romance', 2017, 'Romantic', 'Jin Wook, a rich playboy set to inherit his dad''s company, has a chance encounter with Yoo Mi, who has more modest ambitions to become a nutritionist'),
+#   Kdrama('Inheritors', 2013, 'Romantic', 'After a chance encouter in LA, two teens from different social backgrounds reunite at an exclusive high school attended by Korea''s uber rich'),
+#   Kdrama('Love Alarm', 2021, 'Romantic', 'In a world where an app alerts people if someone in the vicinity likes them, Kim Jojo experiences young love while coping with personal adversities')
+# ]
 
 # Create your views here.
 def home(request):
@@ -23,4 +23,5 @@ def about(request):
   return render(request, 'about.html')
 
 def kdramas_index(request):
+  kdramas = Kdrama.objects.all()
   return render(request,'kdramas/index.html', {'kdramas': kdramas})
