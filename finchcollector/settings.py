97d259db9df196abc5b6ddb6acab99d8e44ddd0c
chedeline <vchedeline@gmail.com>
2022-07-20 12:06:42 -0400

    step 2

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/kdramas/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku
+import django_on_heroku
+django_on_heroku.settings(locals())
